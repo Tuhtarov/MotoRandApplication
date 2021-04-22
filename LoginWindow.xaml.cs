9b25d12d3e89f85585d5470a1c209945d0edfbb3
Alexander Tukhtarov <stuxtarov@mail.ru>
2021-04-23 06:18:25 +0700

    LoginWindow UI

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MotoRandApplication
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(CheckValidField.IsNotEmpty(fieldLogin.Text) && CheckValidField.IsNotEmpty(fieldPassword.Text))
+            {
+                MessageBox.Show("Поля не пустые", "Ввод");
+            }
+            else
+            {
+                MessageBox.Show("Заполните все поля!", "Ввод");
+            }
+        }
+
+        private void loginField_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!CheckValidField.IsUserInputCorrect(e.Text))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ packages/uimanagement/CheckValidField.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MotoRandApplication
+{
+    public class CheckValidField
+    {
+        //проверка вводимых символов в поля авторизации (только латиница и цифры)
+        public static bool IsNotEmpty(string str)
+        {
+            if(str != "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //проверка Данных пользователя (логина или пароля) на отсутсвие спец.символов
+        public static bool IsUserInputCorrect(string LoginOrPswd)
+        {
+            //char[] lp;
+            //lp = LoginOrPswd.ToCharArray();
+            if (Regex.IsMatch(LoginOrPswd, @"^[a-zA-Z0-9]+$"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
